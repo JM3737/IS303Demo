346160027aa2bae7d69305fd736e822390e54fff
realdanewilliams <71400230+realdanewilliams@users.noreply.github.com>
2025-02-20 10:27:56 -0700

    Create multiply.py
    created function to multiply two numbers

---
+++ multiply.py
@@ -0,0 +1,11 @@
+# Dane Williams
+# 02/20/2025
+# This program will multiply two numbers together
+
+def multiply(num1, num2):
+    return num1 * num2
+    
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f'The product of {num1} and {num2} is {multiply(num1, num2)}')
